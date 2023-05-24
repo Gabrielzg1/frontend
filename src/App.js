5234136574322f8eaf94186e78ef300c8ee6ce08
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-24 13:54:52 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Buttons from "./Containers/buttons";
+import { teste } from "./service/api";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Buttons className="buttons" />
-    </div>
-  );
+	const [nome, setnome] = useState("");
+
+	const show = async () => {
+		const response = await teste();
+		setnome(response.data.nome);
+	};
+
+	return (
+		<div className="App">
+			<input type="button" onClick={show}></input>
+			{nome && <h1>{nome}</h1>}
+		</div>
+	);
 }
 
 export default App;
