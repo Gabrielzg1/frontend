bc28a9d2b329fcef0087a88304921ed2cf96ca5f
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-31 08:42:58 -0300

    Update index.jsx

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -3,26 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { getUsers } from "../../services/api";
 
 const MainPage = () => {
-  
+	const loadData = async (query = "") => {
+		try {
+			const response = await getUsers();
+			console.log(response.data);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	useEffect(() => {
+		(async () => await loadData())();
+	}, []);
 
-    const loadData = async (query = '') => {
-        try {
-            const response = await getUsers();
-            console.log(response.data);
-        } catch (err) {
-            console.error(err)
-        }
-    }
-    useEffect(() => {
-        (async () => await loadData())();
-    }, [])
-
-    const navigate = useNavigate();
-    return (
-        <div id="login">
-          <h1>Main Page</h1>
-        <input type="button" value="Teste" onClick={()=> navigate("/login")} />
-        </div>
-    )
-}
+	const navigate = useNavigate();
+	return (
+		<div id="login">
+			<h1>Main Page</h1>
+			<input type="button" value="Teste" onClick={() => navigate("/login")} />
+		</div>
+	);
+};
 export default MainPage;
