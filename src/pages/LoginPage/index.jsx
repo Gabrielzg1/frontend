f387b8cdc1b05ce918bbfe2aa21b3feb4ecd2563
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-05-31 08:41:22 -0300

    bugfix

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -2,69 +2,84 @@ import React, { useState, useContext } from "react";
 import { login } from "../../services/api";
 import { useNavigate } from "react-router-dom";
 import Loader from "../../Containers/Loader";
-import "./styles.css"
+import "./styles.css";
 import Buttons from "../../Containers/Buttons/index";
 
-
 const LoginPage = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false)
-    const [loading, isLoading] = useState(false)
-    const [option, setOption]= useState('')
+	const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
+	const [loading, isLoading] = useState(false);
+	const [option, setOption] = useState("users");
 
-    const handleSelectedOption = (loginType) => {
-        setOption(loginType)
-        console.log(option);
-      };
+	const handleSelectedOption = (loginType) => {
+		setOption(loginType);
+		console.log(option);
+	};
 
-    const hadleLogin = async () => {
-        setError(false)
-        isLoading(true)
-        const response = await login(email, password, option)
-        console.log(response.data);
-        if(response.data.msg == true)
-            navigate("/")
-        else
-            setError(true)
-        isLoading(false)         
-    }
+	const hadleLogin = async () => {
+		setError(false);
+		isLoading(true);
+		const response = await login(email, password, option);
+		console.log(response.data);
+		if (response.data.msg == true) navigate("/");
+		else setError(true);
+		isLoading(false);
+	};
 
-    if(loading){
-        return <Loader/>
-    }
-    return (
-    <div>
-      <div class="login-box">
-      <p>Login</p>
-      <form>
-          <div class="user-box">
-              <input type="email" name="email" id="email" value={email} onChange={(e)=> setEmail(e.target.value)} />
-              <label>Email</label>
-          </div>
-          <div class="user-box">
-              <input type="password" name="password" id="password" value={password} onChange={(e)=>
-              setPassword(e.target.value)} />
-              <label>Password</label>
-          </div>
-          {error == true && <p className="error">Email ou senha inválido</p>}
-          <a onClick={hadleLogin}>
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-              Submit
-          </a>
-      </form>
-      <br />
-      <Buttons handleOptionChange={handleSelectedOption} className="buttons"/>
+	if (loading) {
+		return <Loader />;
+	}
+	return (
+		<div>
+			<div class="login-box">
+				<p>Login</p>
+				<form>
+					<div class="user-box">
+						<input
+							type="text"
+							name="email"
+							id="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<label>Email</label>
+					</div>
+					<div class="user-box">
+						<input
+							type="password"
+							name="password"
+							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<label>Password</label>
+					</div>
+					{error == true && <p className="error">Email ou senha inválido</p>}
+					<a onClick={hadleLogin}>
+						<span></span>
+						<span></span>
+						<span></span>
+						<span></span>
+						Submit
+					</a>
+				</form>
+				<br />
+				<Buttons
+					handleOptionChange={handleSelectedOption}
+					className="buttons"
+				/>
 
-      <p>Don't have an account? <a href="" class="a2">Sign up!</a></p>
-  </div>   
-  
-  </div>
-    )}
+				<p>
+					Don't have an account?{" "}
+					<a href="" class="a2">
+						Sign up!
+					</a>
+				</p>
+			</div>
+		</div>
+	);
+};
 
-
-export default LoginPage
+export default LoginPage;
