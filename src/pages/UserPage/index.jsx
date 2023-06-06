9ac63c3ead0aa452621a42a1025665568ebbed9f
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-06-06 18:01:02 -0300

    colors

--- src/pages/UserPage/index.jsx
+++ src/pages/UserPage/index.jsx
@@ -5,17 +5,29 @@ import "./styles.css";
 import Navbar from "../../Containers/Navbar";
 
 const UserPage = () => {
-	const navigate= useNavigate()
-	const [applied, setApplied] = useState(["Math", "fisica","fisica","fisica","fisica","fisica","fisica","fisica","fisica","fisica","fisica","fisica","fisica"])
-	const [name, setname]=useState("Nome do usuário")
-	const [finished, setFinished] = useState(["Quimica", "fisica"])
-
+	const navigate = useNavigate();
+	const [applied, setApplied] = useState([
+		"Math",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+		"fisica",
+	]);
+	const [name, setname] = useState("Nome do usuário");
+	const [finished, setFinished] = useState(["Quimica", "fisica"]);
 
 	const loadData = async (query = "") => {
 		try {
-			const id = localStorage.getItem("id")
-           	if(id === null)
-		   		navigate("/login")	
+			const id = localStorage.getItem("id");
+			if (id === null) navigate("/login");
 			const response = await getUser(id);
 			//setApplied(response.data.applied)
 			//setname(response.data.username)
@@ -23,7 +35,7 @@ const UserPage = () => {
 			console.log(response.data);
 		} catch (err) {
 			console.error(err);
-			navigate("/login")
+			navigate("/login");
 		}
 	};
 	useEffect(() => {
@@ -32,49 +44,51 @@ const UserPage = () => {
 
 	return (
 		<div id="main">
-            <Navbar/>
-            <h1>Bem vindo, {name}!</h1>
+			<Navbar />
+			<h1 className="title">Bem vindo, {name}!</h1>
 
-
-			<div  id="boxtask1">
-				<ul> <h2>Applied Tasks</h2>
-				<div id="endtask">
-					{
-					applied.map((item) => (
-						<li id="boxtask">
-							<button
-							id="botao"
-							onClick={()=> {navigate("/")}}>
-							{item}
-							</button>
-							
-						</li>
-
-					))}
-				</div>
+			<div id="boxtask1">
+				<ul>
+					{" "}
+					<h2>Applied Tasks</h2>
+					<div id="endtask">
+						{applied.map((item) => (
+							<li id="boxtask">
+								<button
+									id="botao"
+									onClick={() => {
+										navigate("/");
+									}}
+								>
+									{item}
+								</button>
+							</li>
+						))}
+					</div>
 				</ul>
 			</div>
 
-
-			<div  id="boxtask1">
-				<ul> <h2>Finished Tasks</h2>
-				<div id="endtask">
-					{
-					finished.map((item) => (
-						<li id="boxtask">
-							<button
-							id="botao"
-							onClick={()=> {navigate("/")}}>
-							{item}
-							</button>
-						</li>
-					))}
-				</div>
+			<div id="boxtask1">
+				<ul>
+					{" "}
+					<h2>Finished Tasks</h2>
+					<div id="endtask">
+						{finished.map((item) => (
+							<li id="boxtask">
+								<button
+									id="botao"
+									onClick={() => {
+										navigate("/");
+									}}
+								>
+									{item}
+								</button>
+							</li>
+						))}
+					</div>
 				</ul>
 			</div>
-	
 		</div>
-		
 	);
 };
 export default UserPage;
