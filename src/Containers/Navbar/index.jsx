9d981d95d5fa17fa4cb3664c7623298414794776
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2023-06-09 15:25:11 -0300

    feed + treinamento

--- src/Containers/Navbar/index.jsx
+++ src/Containers/Navbar/index.jsx
@@ -4,64 +4,64 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./styles.css";
 
 function Navbar() {
-	const [logged, isLogged] = useState(false);
-	const [type, setType] = useState(null);
-	const navigate = useNavigate();
-	const loadData = async () => {
-		try {
-			const id = localStorage.getItem("id");
-			const option = localStorage.getItem("type");
-			if (option) setType(option);
-			if (id !== null) isLogged(true);
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	useEffect(() => {
-		(async () => await loadData())();
-	}, []);
+  const [logged, isLogged] = useState(false);
+  const [type, setType] = useState(null);
+  const navigate = useNavigate();
+  const loadData = async () => {
+    try {
+      const id = localStorage.getItem("id");
+      const option = localStorage.getItem("type");
+      if (option) setType(option);
+      if (id !== null) isLogged(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    (async () => await loadData())();
+  }, []);
 
-	const logout = async () => {
-		localStorage.clear();
-		navigate("/login");
-	};
+  const logout = async () => {
+    localStorage.clear();
+    navigate("/login");
+  };
 
-	return (
-		<div className="body">
-			<input type="checkbox" id="active" />
-			<label htmlFor="active" className="menu-btn">
-				<span></span>
-			</label>
-			<label htmlFor="active" className="close"></label>
-			<div className="wrapper">
-				<ul>
-					<li>
-						<a href="/home">Home</a>
-					</li>
+  return (
+    <div className="body">
+      <input type="checkbox" id="active" />
+      <label htmlFor="active" className="menu-btn">
+        <span></span>
+      </label>
+      <label htmlFor="active" className="close"></label>
+      <div className="wrapper">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
 
-					{logged === true ? (
-						<>
-							<li>
-								<a href={`/${type}`}>Perfil</a>
-							</li>
-							<li>
-								<a onClick={logout}>Logout</a>
-							</li>
-						</>
-					) : (
-						<>
-							<li>
-								<a href="/register">Register</a>
-							</li>
-							<li>
-								<a href="/login">Login</a>
-							</li>
-						</>
-					)}
-				</ul>
-			</div>
-		</div>
-	);
+          {logged === true ? (
+            <>
+              <li>
+                <a href={`/${type}`}>Perfil</a>
+              </li>
+              <li>
+                <a onClick={logout}>Logout</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <a href="/register">Register</a>
+              </li>
+              <li>
+                <a href="/login">Login</a>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,32 +1,47 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getTrainings, getUsers } from "../../services/api";
+import { getUsers } from "../../services/api";
 import Navbar from "../../Containers/Navbar";
 import "./styles.css";
 
 const HomePage = () => {
-	const [dataError, setDataError] = useState(false);
-	const loadData = async () => {
-		try {
-			const response = await getTrainings();
-			console.log(response.data);
-		} catch (err) {
-			setDataError(true);
-			console.error(err);
-		}
-	};
-	useEffect(() => {
-		(async () => await loadData())();
-	}, []);
+  const [dataError, setDataError] = useState(false);
+  const [trainings, setTrainings] = useState([
+    { name: "Treinamento 1" },
+    { name: "Treinamento 2" },
+    { name: "Treinamento 3" },
+    { name: "Treinamento 4" },
+    { name: "Treinamento 5" },
+    { name: "Treinamento 6" },
+  ]);
+  const loadData = async () => {
+    try {
+      const response = await getUsers();
+      console.log(response.data);
+    } catch (err) {
+      setDataError(true);
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    (async () => await loadData())();
+  }, []);
 
-	const navigate = useNavigate();
-	return (
-		<div className="main-body">
-			<h1 className="main-title">Jobtron</h1>
-			<button className="main-button" onClick={() => navigate("/login")}>
-				Ir para a página de login
-			</button>
-		</div>
-	);
+  const navigate = useNavigate();
+  return (
+    <div className="container">
+      <div className="title">Feed de Treinamento</div>
+      {trainings.map((training, index) => (
+        <div className="training-item" key={index}>
+          <input
+            type="button"
+            className="training-title"
+            value={training.name}
+            onClick={() => navigate("/training")}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 export default HomePage;
